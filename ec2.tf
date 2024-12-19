305d3e5b20c424a67060c1e3f3e109cd3df9cbcb
Adetayo99 <ruffyteewhy@gmail.com>
2024-12-19 05:01:04 +0100

    add ec2 instance and update readme file

--- ec2.tf
+++ ec2.tf
@@ -1,28 +1,49 @@
-variable "ec2_instance_type" {
-  type = string
-  default = "t2.micro"
-}
+resource "aws_instance" "web2" {
 
+  ami           = "ami-0453ec754f44f9a4a"
 
-variable "instance_ami" {
-  type = string
-  default = "ami-0084a47cc718c111a"
-  
+  instance_type = "t2.micro"
+
+ 
+
+  tags = {
+
+    Name = "HelloWorld"
+
+  }
+
 }
-# (1). Declare the provider
 
 terraform {
+
   required_providers {
+
     aws = {
+
       source  = "hashicorp/aws"
-      version = "~>5.0" 
+
+      version = "~> 5.0"
+
     }
+
   }
+
 }
 
-# (2) Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-  profile = "sandbox"
  
+
+# Configure the AWS Provider
+
+provider "aws" {
+
+  region = "us-east-1"
+
+  assume_role {
+
+    role_arn = "arn:aws:iam::970547361695:role/jenkins_admin"
+
+  }
+
+ 
+
 }
